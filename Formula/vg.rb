2f27136ab8313d73c63c357f18aa320ba77430eb
assim <hello@assim.me>
2025-11-26 23:54:31 +0100

    Update vg SHA256 (dry-run + auto-download grammars)

--- Formula/vg.rb
+++ Formula/vg.rb
@@ -6,7 +6,7 @@ class Vg < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/assimelha/vg/releases/download/v0.0.1/vg"
-    sha256 "14a45c8c1980b290bc45e4389846c851575de8553fbfb0af9c892da43564682d"
+    sha256 "5e51592283e96707ce0518f6e45fef7e05a17c1221ec74aabaaf3874efa0c08c"
   else
     # Intel build not yet available
     odie "vg is currently only available for Apple Silicon (arm64)"
