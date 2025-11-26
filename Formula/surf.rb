6df2a1cb05913bdeaf04e688bdd035317878e9b8
assim <hello@assim.me>
2025-11-26 17:16:53 +0100

    Update surf to v1.1.0 - add persistent session mode

--- Formula/surf.rb
+++ Formula/surf.rb
@@ -1,15 +1,15 @@
 class Surf < Formula
   desc "Web browser for LLMs - converts pages to markdown with JS execution"
   homepage "https://github.com/assimelha/surf"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/assimelha/surf/releases/download/v1.0.0/surf-darwin-arm64"
-    sha256 "472d57d850f109c26ba3abf0bbab03cfee68c0abb31ebf3a8616afbbc87214b1"
+    url "https://github.com/assimelha/surf/releases/download/v1.1.0/surf-darwin-arm64"
+    sha256 "5550945453261e12d4263849b7540e7e106ca6c495fb83311ed9a9ea327798d5"
   else
-    url "https://github.com/assimelha/surf/releases/download/v1.0.0/surf-darwin-amd64"
-    sha256 "5723cf685ff6e5e6b4b0fc6cc059f8972a321f7168964cc816f2025beffd2108"
+    url "https://github.com/assimelha/surf/releases/download/v1.1.0/surf-darwin-amd64"
+    sha256 "974291408061d7d1993bade07d371badc119648bb036fe0379d45c60f062d4cc"
   end
 
   def install
