65c5bf62e7e9b54a68d5e07f79d8fe8943bb5f12
assim <hello@assim.me>
2025-11-27 01:06:44 +0100

    Update vg SHA256 (fix watcher + timeout)

--- Formula/vg.rb
+++ Formula/vg.rb
@@ -6,7 +6,7 @@ class Vg < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/assimelha/vg/releases/download/v0.0.1/vg"
-    sha256 "6df3ab804a04fb7d6923b569ba731aae574ed617dee707529d5cfc6e6a583671"
+    sha256 "c7958dde1d96e0fb9ee6056d3b227d124b641754108695e8c26e72b0fe6e6db5"
   else
     # Intel build not yet available
     odie "vg is currently only available for Apple Silicon (arm64)"
