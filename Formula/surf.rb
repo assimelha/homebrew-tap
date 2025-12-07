5b3d5f3aba25af5847435acb02d542e1589c3e68
assim <hello@assim.me>
2025-12-07 16:04:29 +0100

    surf 1.4.0

--- Formula/surf.rb
+++ Formula/surf.rb
@@ -1,15 +1,15 @@
 class Surf < Formula
   desc "Web browser for LLMs - converts pages to markdown with JS execution"
   homepage "https://github.com/assimelha/surf"
-  version "1.3.0"
+  version "1.4.0"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/assimelha/surf/releases/download/v1.3.0/surf-darwin-arm64"
-    sha256 "318ffeb769550b90b6e7eb7ab0e96975f4d4d872252a2ca4f5e45ca36431803f"
+    url "https://github.com/assimelha/surf/releases/download/v1.4.0/surf-darwin-arm64"
+    sha256 "a5d4ec126937183efa8cfe12028e9509c6628fb8414e0aa8fb825477b12f8396"
   else
-    url "https://github.com/assimelha/surf/releases/download/v1.3.0/surf-darwin-amd64"
-    sha256 "ce00b7e7181938b9764de4f9ce4a22112461dc9157208cd0310f0d4932b5bf8f"
+    url "https://github.com/assimelha/surf/releases/download/v1.4.0/surf-darwin-amd64"
+    sha256 "93c851841d462e7ebdc8036feecd599817479e4b34c7862bb6e515ab2be842a6"
   end
 
   def install
